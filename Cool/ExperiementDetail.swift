30ac55e4b8cb3defff611b63802cbd40488ceab5
CostardRouge <mncorp971@gmail.com>
2016-03-14 17:41:37 +0100

    simple experiment detail class added

---
+++ Cool/ExperiementDetail.swift
@@ -0,0 +1,26 @@
+//
+//  ExperiementDetail.swift
+//  Cool
+//
+//  Created by Steeve Pommier on 24/12/15.
+//  Copyright © 2015 Steeve is working. All rights reserved.
+//
+
+import Foundation
+
+class ExperimentDetail {
+    var name: String = ""
+    var author: String? = ""
+    
+    var description: String? = ""
+    var howToUseIt: String? = ""
+    
+    var inspirationSource: String? = ""
+    var repositoryURL: String? = ""
+    
+    var creationDate: NSDate = NSDate()
+    var lastVersionDate: NSDate = NSDate()
+    
+    let thumbnailImageNamed: String = ""
+}
+
